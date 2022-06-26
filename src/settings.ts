6fc142a17cc7a5fe1aa3653ba21becc529dcdc37
Chiye <huangchiye@gmail.com>
2022-06-26 08:48:40 -0400

    clean up and cross chain support for gnosis chain

--- src/settings.ts
+++ src/settings.ts
@@ -1,17 +1,25 @@
-import { Config, Mumbai as _Mumbai } from "@usedapp/core";
-import { Chain } from "@usedapp/core";
+import { Config, Mumbai as _Mumbai } from '@usedapp/core';
+import { Chain } from '@usedapp/core';
 
-type SafeChain = Chain & { rpcUrl: string; networkName: string; chainId: number};
+type SafeChain = Chain & {
+  rpcUrl: string;
+  networkName: string;
+  chainId: number;
+};
 
-const MUMBAI_RPC_URL = "https://rpc-mumbai.maticvigil.com";
-const MUMBAI_NETWORK_NAME = "polygon-mumbai";
+const MUMBAI_RPC_URL = 'https://rpc-mumbai.maticvigil.com';
+const MUMBAI_NETWORK_NAME = 'polygon-mumbai';
 const MUMBAI_CHAIN_ID = 80001;
+
+const GNOSIS_RPC_URL = 'https://rpc.ankr.com/gnosis';
+const GNOSIS_NETWORK_NAME = 'gnosis-chain';
+const GNOSIS_CHAIN_ID = 100;
 
 const Mumbai: SafeChain = {
   ..._Mumbai,
   rpcUrl: MUMBAI_RPC_URL,
   networkName: MUMBAI_NETWORK_NAME,
-  chainId: MUMBAI_CHAIN_ID
+  chainId: MUMBAI_CHAIN_ID,
 };
 
 export const ActiveChain: SafeChain = Mumbai;
@@ -32,17 +40,17 @@ export const walletConnectOptions = {
 };
 
 export const factoryInfo = {
-  address: "0x0454603a57C72f53f7a078ce52c7E7ADD4201D47",
+  address: '0x0454603a57C72f53f7a078ce52c7E7ADD4201D47',
   deployBlock: 26913073,
 };
 
 export const superfluidHostInfo = {
-  address: "0xEB796bdb90fFA0f28255275e16936D25d3418603",
+  address: '0xEB796bdb90fFA0f28255275e16936D25d3418603',
 };
 
 export const superfluidTokenFactoryInfo = {
-  address: "0x200657E2f123761662567A1744f9ACAe50dF47E6",
+  address: '0x200657E2f123761662567A1744f9ACAe50dF47E6',
 };
 
 export const GRAPH_API =
-  "https://api.thegraph.com/subgraphs/name/ewitulsk/allo";
+  'https://api.thegraph.com/subgraphs/name/ewitulsk/allo';
